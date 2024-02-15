102dff6017b2fc11aa914b5227c0ef29d8ed38c2
Marcos Riveros <marcosriverosk@gmail.com>
2024-02-15 19:01:11 -0300

    parameterized users

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # WIZE - Automation Test
 
-##
+### Run tests
+
+Before run the tests execute:
+
+Install dependencies
+
+```
+yarn install
+```
+
+Install playwright browsers
+
+```
+yarn playwright install
+```
+
+Execute tests
+
+```
+yarn test
+```
+
 
 ### Before Commit
 
--- tests/saucedemoLogin.spec.js
+++ tests/saucedemoLogin.spec.js
@@ -1,28 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { Login } = require('../src/pages/login.page.js');
 
-const users = [
-  { userNae: 'asdf', password: 'screte'}
-  //...
-]
-
-
-for(const user of users) {
-  test('test', async ({ page }) => {
-
-    await page.goto('https://www.saucedemo.com/');
-    const loginPage = new Login(page);
-  
-    await loginPage.fillUserName(user.userNae);
-    await loginPage.fillPassword(user.password);
-    await loginPage.clickLoginBtn();
-  
-    expect(await page.locator('text=Products')).toBeVisible();
-    expect(page.url()).toContain('inventory')
-  });
-}
-
-test('test', async ({ page }) => {
+test('test login', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
   const loginPage = new Login(page);
@@ -35,3 +14,28 @@ test('test', async ({ page }) => {
   expect(page.url()).toContain('inventory')
 });
 
+
+// Parameterized version
+
+const users = [
+  { userNae: 'standard_user', password: 'secret_sauce'},
+  { userNae: 'error_user', password: 'secret_sauce'},
+  { userNae: 'visual_user', password: 'secret_sauce'},
+  // ... more users
+]
+
+
+for(const user of users) {
+  test(`Test user: ${user.userNae}`, async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+    const loginPage = new Login(page);
+  
+    await loginPage.fillUserName(user.userNae);
+    await loginPage.fillPassword(user.password);
+    await loginPage.clickLoginBtn();
+  
+    expect(await page.locator('text=Products')).toBeVisible();
+    expect(page.url()).toContain('inventory')
+  });
+}
